0845b4849dae2ed6da5be1c98e9585a134654db4
Tushar Ahlawat <128771341+tusharahlawat@users.noreply.github.com>
2025-06-29 18:01:33 +0530

    Time: 0 ms (100%), Space: 20.4 MB (6.96%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -4,9 +4,7 @@ public:
         if (head == nullptr) return nullptr;
         head->next = removeElements(head->next, val);
         if (head->val == val) {
-            ListNode* nextNode = head->next;
-            //delete head;  // free memory in C++
-            return nextNode;
+            return head->next;
         } else {
             return head;
         }
