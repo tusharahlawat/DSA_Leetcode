31620133e12c73cfc6c2af3278ee45aee66e45b4
Tushar Ahlawat <128771341+tusharahlawat@users.noreply.github.com>
2025-07-17 23:32:19 +0530

    Time: 10 ms (99.13%), Space: 13 MB (89.27%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.py
+++ 0838-design-linked-list/0838-design-linked-list.py
@@ -1,60 +1,61 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+class MyLinkedList(object):
 
-class MyLinkedList:
     def __init__(self):
-        self.head = None
-        self.size = 0
+        self.buf = []
 
     def get(self, index):
-        if index < 0 or index >= self.size:
-            return -1
-        current = self.head
-        for _ in range(index):
-            current = current.next
-        return current.val
+        """
+        :type index: int
+        :rtype: int
+        """
+
+        if index < 0 or index > len(self.buf) - 1: return -1
+        else: return self.buf[index]
+        
 
     def addAtHead(self, val):
-        new_node = Node(val)
-        new_node.next = self.head
-        self.head = new_node
-        self.size += 1
+        """
+        :type val: int
+        :rtype: None
+        """
+
+        self.buf.insert(0, val)
+        
 
     def addAtTail(self, val):
-        new_node = Node(val)
-        if not self.head:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
-        self.size += 1
+        """
+        :type val: int
+        :rtype: None
+        """
+
+        self.buf.append(val)
+        
 
     def addAtIndex(self, index, val):
-        if index < 0 or index > self.size:
-            return
-        if index == 0:
-            self.addAtHead(val)
-            return
-        new_node = Node(val)
-        current = self.head
-        for _ in range(index - 1):
-            current = current.next
-        new_node.next = current.next
-        current.next = new_node
-        self.size += 1
+        """
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
+
+        if index <= len(self.buf):
+            self.buf.insert(index, val)
+        
 
     def deleteAtIndex(self, index):
-        if index < 0 or index >= self.size:
-            return
-        if index == 0:
-            self.head = self.head.next
-        else:
-            current = self.head
-            for _ in range(index - 1):
-                current = current.next
-            current.next = current.next.next
-        self.size -= 1
+        """
+        :type index: int
+        :rtype: None
+        """
+
+        if index < len(self.buf): self.buf.pop(index)
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
