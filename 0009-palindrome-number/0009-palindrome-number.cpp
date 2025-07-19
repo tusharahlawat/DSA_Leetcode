d9d8995e0451acb1e31ac09712e7b48d04ab6466
Tushar Ahlawat <128771341+tusharahlawat@users.noreply.github.com>
2025-07-19 22:51:07 +0530

    Time: 6 ms (18.82%), Space: 11 MB (9.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string y=to_string(x);
+        if(x<0){
+            return false;
+        }
+        else{
 
         for(int i = 0;i<(y.length()/2)+1;i++){
                 if(y[i]!=y[y.length()-1-i]){
                     return false;
                 }
-            }
+            }}
         return true;
     }
 };
