2041fe1676743f792311c13516cfaefad4ae8f21
Tushar Ahlawat <128771341+tusharahlawat@users.noreply.github.com>
2025-07-20 00:14:37 +0530

    Time: 227 ms (8.36%), Space: 10.2 MB (99.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,14 +6,14 @@ public:
         
         for(int i = 0;i<n;i++){
             for(int j=i;j<n;j++){
-                bool res = true;
+                int res = 1;
                 for(int k = i ; k<j;k++){
                     if(s[k]==s[j]){
-                        res = false;
+                        res = 2;
                         break;
                     }
                 }
-                if(res==true){
+                if(res==1){
                     maxLen=max(maxLen,j-i+1);
                 }
                 else{
