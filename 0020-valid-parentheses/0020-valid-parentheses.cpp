6ef7dfc98ec7ba792ea70fb8e640c15266cbe869
Tushar Ahlawat <128771341+tusharahlawat@users.noreply.github.com>
2025-07-20 00:52:53 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.95%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stack;
-        unordered_map<char, char> mapping = {{')', '('}, {']', '['}, {'}', '{'}};
-
-        for (char c : s) {
-            if (mapping.find(c) == mapping.end()) {
-                stack.push(c);
-            } else if (!stack.empty() && mapping[c] == stack.top()) {
-                stack.pop();
-            } else {
+        stack <char> strr;
+        for(int i = 0;i<s.length();i++){
+        if(s[i]=='[' || s[i]=='(' || s[i]=='{' ){
+            strr.push(s[i]);
+        }
+        else {
+            if(strr.empty()){
                 return false;
             }
+            if ((s[i]==']' && strr.top()!='[')||(s[i]=='}' && strr.top()!='{')||(s[i]==')' && strr.top()!='(')){
+            return false;
+                }
+        else{
+            strr.pop();
         }
-
-        return stack.empty();        
+        }
+        }
+        return strr.empty();
     }
 };
