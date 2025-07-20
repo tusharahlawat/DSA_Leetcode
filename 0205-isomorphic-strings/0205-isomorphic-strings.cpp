be7ffe621d52a178691f4f4055694d5f297e31d5
Tushar Ahlawat <128771341+tusharahlawat@users.noreply.github.com>
2025-07-21 00:31:48 +0530

    Time: 1334 ms (7.04%), Space: 9.1 MB (98.42%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        int i =0;
+        if(n!=m) {
+            return false;
+            }
+        
+        else{
+            for(int i = 0;i<n;i++){
+                for(int j = i+1;j<n;j++){
+                if((s[i]==s[j])&&(t[i]!=t[j])){
+                    return false;
+                }
+                if((s[i]!=s[j])&&(t[i]==t[j])){
+                    return false;
+                }
+                }
+            }
+            
+        }
+        return true;
+    }
+};
